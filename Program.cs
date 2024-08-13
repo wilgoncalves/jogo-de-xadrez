1454b732fdb68de885f647dd0c83c3ee4b093344
Willian Gonçalves <santoswil81@gmail.com>
2024-08-13 15:18:04 -0300

    Criando a classe PosicaoXadrez

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@
 using xadrez_console;
 using pecas_xadrez;
 
-try
-{
-    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+//try
+//{
+//    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-    tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-    tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-    tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 2));
+//    tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+//    tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+//    tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 2));
 
-    Tela.ImprimirTabuleiro(tabuleiro);
-}
-catch(TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
+//    Tela.ImprimirTabuleiro(tabuleiro);
+//}
+//catch(TabuleiroException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+PosicaoXadrez posicao = new PosicaoXadrez('c', 7);
+
+Console.WriteLine(posicao);
+Console.WriteLine(posicao.ConversorPosicao());
---
+++ pecas-xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+
+namespace pecas_xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ConversorPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
